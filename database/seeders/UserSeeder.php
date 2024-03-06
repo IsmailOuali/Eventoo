8b12b81e84699594931636e979a3d66148bee576
unknown <ismail.ouali@ump.ac.ma>
2024-03-06 14:49:39 +0100

    seed only one

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use DB;
 
 class UserSeeder extends Seeder
 {
@@ -26,11 +27,11 @@ class UserSeeder extends Seeder
                 'role' => 'Organisateur'
             ],
             [
-                'name' => 'TestAdmin',
-                'email' => 'admin@gmail.com',
+                'name' => 'Testclient',
+                'email' => 'client@gmail.com',
                 'password' => bcrypt('Root12345'),
-                'role' => 'admin'
-            ],
+                'role' => 'client'
+            ]
 
         );
     }
