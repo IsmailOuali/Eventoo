69a6c700f39bde9f2adddbd13f6076a43c35817f
unknown <ismail.ouali@ump.ac.ma>
2024-03-06 15:30:45 +0100

    Starting the front-end

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,184 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+  
+    <div class="flex flex-col h-screen bg-gray-100">
+    
+        <!-- Contenido principal -->
+        <div class="flex-1 flex flex-wrap">
+            <!-- Barra lateral de navegación (oculta en dispositivos pequeños) -->
+            <div class="p-2 bg-white w-full md:w-60 flex flex-col md:flex hidden" id="sideNav">
+                <nav>
+                    <a class="block text-gray-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white" href="#">
+                        <i class="fas fa-home mr-2"></i>Statistiques sur mes evenements
+                    </a>
+                    <a class="block text-gray-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white" href="#">
+                        <i class="fas fa-file-alt mr-2"></i>Gestion des evenements
+                    </a>
+                    <a class="block text-gray-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white" href="#">
+                        <i class="fas fa-store mr-2"></i>Acceptation de demande
+                    </a>
+                </nav>
+    
+                <a class="block text-gray-500 py-2.5 px-4 my-2 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white mt-auto" href="#">
+                    <i class="fas fa-sign-out-alt mr-2"></i>Cerrar sesión
+                </a>
+    
+                <div class="bg-gradient-to-r from-cyan-300 to-cyan-500 h-px mt-2"></div>
+    
+                <p class="mb-1 px-5 py-3 text-left text-xs text-cyan-500">Copyright OualiIsmail@2024</p>
+    
+            </div>
+    
+            <div class="flex-1 p-4 w-full w-full">
+    
+                     <!-- Contenedor de Gráficas -->
+                <div class="mt-8 flex flex-wrap space-x-0 space-y-2 md:space-x-4 md:space-y-0">
+
+                    <div class="flex-1 bg-white p-4 shadow rounded-lg w-full">
+                        <h2 class="text-gray-500 text-lg font-semibold pb-1">Stats</h2>
+                        <div class="my-1"></div> <!-- Espacio de separación -->
+                        <div class="bg-gradient-to-r from-cyan-300 to-cyan-500 h-px mb-6"></div> <!-- Línea con gradiente -->
+                        <div class="chart-container" style="position: relative; height:150px; width:100%">
+                            <!-- El canvas para la gráfica -->
+                            <canvas id="commercesChart"></canvas>
+                        </div>
+                    </div>
+                </div>
+    
+                <!-- Sección 3 - Tabla de Autorizaciones Pendientes -->
+                <div class="mt-8 bg-white p-4 shadow rounded-lg">
+                    <h2 class="text-gray-500 text-lg font-semibold pb-4">Vos evenements</h2>
+                    <div class="my-1"></div> <!-- Espacio de separación -->
+                    <div class="bg-gradient-to-r from-cyan-300 to-cyan-500 h-px mb-6"></div> <!-- Línea con gradiente -->
+                    <table class="w-full table-auto text-sm">
+                        <thead>
+                            <tr class="text-sm leading-normal">
+                                <th class="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light">#</th>
+                                <th class="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light">Name</th>
+                                <th class="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light">Place</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="hover:bg-grey-lighter">
+                                <td class="py-2 px-4 border-b border-grey-light"><img src="https://via.placeholder.com/40" alt="Foto Perfil" class="rounded-full h-10 w-10"></td>
+                                <td class="py-2 px-4 border-b border-grey-light">Tomorrow Land</td>
+                                <td class="py-2 px-4 border-b border-grey-light">Belguim</td>
+                            </tr>
+                            <!-- Añade más filas aquí como la anterior para cada autorización pendiente -->
+                            <tr class="hover:bg-grey-lighter">
+                                <td class="py-2 px-4 border-b border-grey-light"><img src="https://via.placeholder.com/40" alt="Foto Perfil" class="rounded-full h-10 w-10"></td>
+                                <td class="py-2 px-4 border-b border-grey-light">Mawazin</td>
+                                <td class="py-2 px-4 border-b border-grey-light">Morocco</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <!-- Botón "Ver más" para la tabla de Autorizaciones Pendientes -->
+                    <div class="text-right mt-4">
+                        <button class="bg-cyan-500 hover:bg-cyan-600 text-white font-semibold py-2 px-4 rounded">
+                            Voir Plus
+                        </button>
+                    </div>
+                </div>
+    
+                <!-- Cuarto contenedor -->
+                <!-- Sección 4 - Tabla de Transacciones -->
+                <div class="mt-8 bg-white p-4 shadow rounded-lg">
+                    <div class="bg-white p-4 rounded-md mt-4">
+                        <h2 class="text-gray-500 text-lg font-semibold pb-4">Demande d'acceptation</h2>
+                        <div class="my-1"></div> <!-- Espacio de separación -->
+                        <div class="bg-gradient-to-r from-cyan-300 to-cyan-500 h-px mb-6"></div> <!-- Línea con gradiente -->
+                        <table class="w-full table-auto text-sm">
+                            <thead>
+                                <tr class="text-sm leading-normal">
+                                    <th class="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light">Nombre</th>
+                                    <th class="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light">Fecha</th>
+                                    <th class="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-right">Monto</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="hover:bg-grey-lighter">
+                                    <td class="py-2 px-4 border-b border-grey-light">Carlos Sánchez</td>
+                                    <td class="py-2 px-4 border-b border-grey-light">27/07/2023</td>
+                                    <td class="py-2 px-4 border-b border-grey-light text-right">$1500</td>
+                                </tr>
+                            <tr class="hover:bg-grey-lighter">
+                                <td class="py-2 px-4 border-b border-grey-light">Pedro Hernández</td>
+                                <td class="py-2 px-4 border-b border-grey-light">02/08/2023</td>
+                                <td class="py-2 px-4 border-b border-grey-light text-right">$1950</td>
+                            </tr>
+                            <tr class="hover:bg-grey-lighter">
+                                <td class="py-2 px-4 border-b border-grey-light">Sara Ramírez</td>
+                                <td class="py-2 px-4 border-b border-grey-light">03/08/2023</td>
+                                <td class="py-2 px-4 border-b border-grey-light text-right">$1850</td>
+                            </tr>
+                            <tr class="hover:bg-grey-lighter">
+                                <td class="py-2 px-4 border-b border-grey-light">Daniel Torres</td>
+                                <td class="py-2 px-4 border-b border-grey-light">04/08/2023</td>
+                                <td class="py-2 px-4 border-b border-grey-light text-right">$2300</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <!-- Botón "Ver más" para la tabla de Transacciones -->
+                        <div class="text-right mt-4">
+                            <button class
+                                                    <div class="text-right mt-4">
+                                <button class="bg-cyan-500 hover:bg-cyan-600 text-white font-semibold py-2 px-4 rounded">
+                                    Ver más
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    
+    <!-- Script para las gráficas -->
+    <script>
+        // Gráfica de Usuarios
+        var usersChart = new Chart(document.getElementById('usersChart'), {
+            type: 'doughnut',
+            data: {
+                labels: ['Nuevos', 'Registrados'],
+                datasets: [{
+                    data: [30, 65],
+                    backgroundColor: ['#00F0FF', '#8B8B8D'],
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                legend: {
+                    position: 'bottom' // Ubicar la leyenda debajo del círculo
+                }
+            }
+        });
+    
+        // Gráfica de Comercios
+        var commercesChart = new Chart(document.getElementById('commercesChart'), {
+            type: 'doughnut',
+            data: {
+                labels: ['Nuevos', 'Registrados'],
+                datasets: [{
+                    data: [60, 40],
+                    backgroundColor: ['#FEC500', '#8B8B8D'],
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                legend: {
+                    position: 'bottom' // Ubicar la leyenda debajo del círculo
+                }
+            }
+        });
+    
+        // Agregar lógica para mostrar/ocultar la navegación lateral al hacer clic en el ícono de menú
+        const menuBtn = document.getElementById('menuBtn');
+        const sideNav = document.getElementById('sideNav');
+    
+        menuBtn.addEventListener('click', () => {
+            sideNav.classList.toggle('hidden'); // Agrega o quita la clase 'hidden' para mostrar u ocultar la navegación lateral
+        });
+    </script>
 </x-app-layout>
