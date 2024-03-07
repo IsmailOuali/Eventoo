38e1c7adebf08db4fe60df0ac7c1397b1449f118
unknown <ismail.ouali@ump.ac.ma>
2024-03-07 17:02:35 +0100

    Front-end home page

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 
 class User extends Authenticatable
 {
-    use HasFactory;
-    protected $guarded = [];  
+    use HasFactory, HasRoles;
+    protected $guarded = [];
 
 }
