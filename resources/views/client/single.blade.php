1efa8fa3de21cd71e40b08b1aeaf6a7e953cf7f6
unknown <ismail.ouali@ump.ac.ma>
2024-03-12 11:11:16 +0000

    Justifying the front-end

--- resources/views/client/single.blade.php
+++ resources/views/client/single.blade.php
@@ -62,61 +62,49 @@
     </header>
 
     <div>
-    <div class="bg-gray-100 dark:bg-gray-800 py-8">
-        <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="flex flex-col md:flex-row -mx-4">
-                <div class="md:flex-1 px-4">
-                    <!-- Event Image -->
-                    <div class="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
-                        <img class="w-full h-full object-cover" src="" alt="Event Image">
-                    </div>
-                    <!-- Add to Cart Button -->
-                    <div class="flex -mx-2 mb-4">
-                        <div class="w-1/2 px-2">
-                        <a href="{{ route('client.createReservation', ['id' => $event->id]) }}" class="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Reserve a Seat</a>
+        <div class="bg-gray-100 dark:bg-gray-800 py-8">
+            <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="flex flex-col md:flex-row -mx-4">
+                    <div class="md:flex-1 px-4">
+                        <!-- Event Image -->
+                        <div class="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
+                            <img class="w-full h-full object-cover" src="" alt="Event Image">
+                        </div>
+                        <!-- Add to Cart Button -->
+                        <div class="flex -mx-2 mb-4">
+                            <div class="w-1/2 px-2">
+                            <a href="{{ route('client.createReservation', ['id' => $event->id]) }}" class="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Reserve a Seat</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="md:flex-1 px-4">
-                    <!-- Event Title -->
-                    <h2 class="text-2xl font-bold text-gray-800 dark:text-white mb-2">{{ $event->title }}</h2>
-                    <!-- Event Description -->
-                    
-                    <!-- Event Details -->
-                    <div class="flex mb-4">
-                        <div class="mr-4">
-                            <span class="font-bold text-gray-700 dark:text-gray-300">Price:</span>
-                            <span class="text-gray-600 dark:text-gray-300">$</span>
+                    <div class="md:flex-1 px-4">
+                        <!-- Event Title -->
+                        <h2 class="text-2xl font-bold text-gray-800 dark:text-white mb-2">{{ $event->title }}</h2>
+                        <!-- Event Description -->
+                        
+                        <!-- Event Details -->
+                        <div class="flex mb-4">
+                            <div class="mr-4">
+                                <span class="font-bold text-gray-700 dark:text-gray-300">Category:</span>
+                                <span class="text-gray-600 dark:text-gray-300">{{ $event->category->name }}</span>
+                            </div>
+                            <div>
+                                <span class="font-bold text-gray-700 dark:text-gray-300">Availability:</span>
+                                <span class="text-gray-600 dark:text-gray-300">{{ $event->available_seats }}</span>
+                            </div>
                         </div>
+                        <!-- Event Description -->
                         <div>
-                            <span class="font-bold text-gray-700 dark:text-gray-300">Availability:</span>
-                            <span class="text-gray-600 dark:text-gray-300">{{ $event->available_seats }}</span>
+                            <span class="font-bold text-gray-700 dark:text-gray-300">Event Description:</span>
+                            <p class="text-gray-600 dark:text-gray-300 text-sm mt-2">
+                                {{ $event->description }}
+                            </p>
                         </div>
-                    </div>
-                    <!-- Event Description -->
-                    <div>
-                        <span class="font-bold text-gray-700 dark:text-gray-300">Event Description:</span>
-                        <p class="text-gray-600 dark:text-gray-300 text-sm mt-2">
-                            {{ $event->description }}
-                        </p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-
-
-    <footer class="px-4 pt-12 pb-8 text-white bg-white border-t border-gray-200">
-        
-             
-        <div class=" text-center text-gray-500 border-t border-gray-100">
-            © 2024 Ouali Ismail. All rights reserved.
-        </div>
-
-    </footer>
-
-
     <script>
         if (document.getElementById('nav-mobile-btn')) {
             document.getElementById('nav-mobile-btn').addEventListener('click', function () {
