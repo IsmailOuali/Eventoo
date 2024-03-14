7abe78530a939f52ddd7a5e15c33e2463ddc88ea
unknown <ismail.ouali@ump.ac.ma>
2024-03-14 09:56:00 +0000

    Adding the test.js

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
 let colors =["Red", "Green", "White", "Black"];
 
 let color;
-console.log();
 for(let i = 0;colors;i++){
     color = colors[i];
-    console.log(color + ',');
+    console.log(`${color},`);
 }
